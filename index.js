f8d31d5ac28df874772cb5bec8709c19e74e969b
Daniel Yoshimatsu <danielyoshipele@gmail.com>
2024-03-06 18:29:24 -0300

    ahaha

--- index.js
+++ index.js
@@ -1,118 +1,50 @@
 const express = require("express");
 const app = express();
-const amqp = require("amqplib");
-const playwright = require("playwright");
+const bodyparser = require("body-parser");
+
+function parseJwt(token) {
+  return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+}
 
 (async () => {
-  const queue = "tasks";
-  const conn = await amqp.connect("amqp://localhost");
-
-  const ch1 = await conn.createChannel();
-  await ch1.assertQueue(queue);
-
-  ch1.consume(queue, async (msg) => {
-    if (msg !== null) {
-      const msgContent = msg.content.toString();
-      console.log("getUserId:", msgContent);
-      // ch1.ack(msg);
-
-      try {
-        const browser = await playwright["chromium"].launch({
-          headless: true,
-        });
-        const context = await browser.newContext();
-        const page = await context.newPage();
-        page.setDefaultTimeout(5000);
-
-        try {
-          await page.goto("https://login.gupy.io/candidates/curriculum");
-        } catch (e) {
-          console.log("test");
-          await browser.close();
-          return;
-        }
-
-        try {
-          await page.waitForSelector("#radix-0 > div > button");
-          await page.click("#radix-0 > div > button");
-        } catch (e) {
-          console.log(e);
-        }
-
-        try {
-          await page.waitForSelector("#password-access-button");
-          await page.click(
-            "#password-access-button > span > div > span:nth-child(2)"
-          );
-
-          console.log("shouldve been clicked");
-        } catch (e) {
-          console.log(e);
-        }
-
-        await page.waitForSelector("input[id=username]");
-        await page
-          .getByPlaceholder("email@email.com")
-          .fill("testgupyhaha@mailinator.com");
-
-        await page
-          .getByPlaceholder("Enter your password here")
-          .fill("Devtest123@");
-
-        try {
-          await page.click(
-            "#candidates-root > div > div > div > div > form > button > span > div > span"
-          );
-
-          await page.click(
-            "#candidates-root > div > div > div > header > div > div > button > div"
-          );
-        } catch (e) {
-          console.log(e);
-        }
-
-        try {
-          await page.click(
-            "#__next > div > header > div > div > div.sc-2e8a24f-3.imizfz > div > div > button > span > div > div"
-          );
-
-          await page.click("#overlay-menu > div > div > a:nth-child(4)");
-        } catch (e) {
-          console.log(e);
-        }
-
-        await page.click("body > div > div > div > div > button:nth-child(2)");
-
-        await page.route("**/*", async (route, request) => {
-          const response = await request.url();
-          const numbers = response.match(/\b\d{8,10}\b/g);
-
-          if (response.includes("private-api.gupy.io") && numbers) {
-            console.log(numbers[0]);
-            await browser.close();
-            return;
-          }
-          route.continue();
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      console.log("Consumer cancelled by server");
+  app.use(bodyparser.json());
+  app.post("/generatetoken", async (req, res) => {
+    if (!req.body.email && !req.body.password) {
+      res.sendStatus(403);
+      return;
     }
-  });
-  const ch2 = await conn.createChannel();
-
-  app.post("/validate", (req, res) => {
-    if (!req.headers.id) res.sendStatus(403);
-    const id = req.headers.id;
 
     try {
-      ch2.sendToQueue(queue, Buffer.from(`${id}`));
-      res.sendStatus(200);
+      const authResponse = await fetch(
+        "https://private-api.gupy.io/authentication/v2/candidate/account/signin",
+        {
+          body: `{"username":"${req.body.email}","password":"${req.body.password}","subdomain":"login"}`,
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      );
+
+      const setCookie = authResponse.headers
+        .getSetCookie()
+        .splice(0, 1)[0]
+        .split(";")[0]
+        .split("=")[1];
+
+      const userId = parseJwt(setCookie);
+
+      res.send({ setCookie, ...userId });
     } catch (e) {
-      console.error(e);
-      res.sendStatus(404);
+      console.log(e);
+      res.sendStatus(401);
+    }
+  });
+
+  app.get("/applications", async (req, res) => {
+    if (!req.headers("authorization")) {
+      res.sendStatus(403);
+      // return
     }
   });
 
